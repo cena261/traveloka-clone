91a09a3c87a50b240bee8a9813202b193530ebab
cena261 <khanhtu261@gmail.com>
2025-08-28 20:25:24 +0700

    chore: initial commit

---
+++ backend/src/main/java/com/cena/traveloka/TravelokaApplication.java
@@ -0,0 +1,13 @@
+package com.cena.traveloka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelokaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cena/traveloka/TravelokaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cena.traveloka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelokaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
